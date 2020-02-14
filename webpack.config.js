55cb5f19ae7676ac4aa2074d260b182ab23215d3
Aguirre Sabino <aguirresabino187@gmail.com>
2020-02-14 00:48:26 -0300

    Adiciona configuração de webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,16 @@ const path = require('path');
 
 module.exports = {
   mode: `${process.env.NODE_ENV}`,
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: 'index.js',
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
   }
 }
