f29bfad7f7d776eefff373b15f377121f9cd1d4b
Aguirre Sabino <aguirresabino187@gmail.com>
2020-02-14 17:38:08 -0300

    Remove .babelrc
    Define presets do babel em webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,9 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
+      query: {
+        presets: ['@babel/preset-env', '@babel/react']
+      }
     }]
   },
 }
