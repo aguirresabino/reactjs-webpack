216a30447d78eafd3ada1cd97429448c968948d0
Aguirre Sabino <aguirresabino187@gmail.com>
2020-02-14 11:25:05 -0300

    Modulariza componente App

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App(props) {
+    return (
+        <div>
+            <h1>{props.value}</h1>
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
-
-function App(props) {
-    return (
-        <div>
-            <h1>App works!</h1>
-        </div>
-    );
-};
+import { render } from 'react-dom';
+import App from './App';
 
 render(
-    <App/>,
+    <App value='App works!!'/>,
     document.getElementById('root')
 );
