ea7fd2ac7ddeb10af141d0c7c3e7df658f031438
Aguirre Sabino <aguirresabino187@gmail.com>
2020-02-14 00:18:12 -0300

    Define 'mode' em webpack.config.js de acordo com o a variável de ambiente NODE_ENV
    Alterar pasta de saída para 'public'

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: `${process.env.NODE_ENV}`,
   entry: path.join(__dirname, 'src', 'index'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   }
 }
